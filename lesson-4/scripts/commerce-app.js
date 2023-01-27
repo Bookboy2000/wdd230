63bccf091d66133c8c09956ab9732ab740a7a111
Bookboy2000 <wil21131@byui.edu>
2023-01-27 13:08:13 -0700

    Changing things to look better

--- lesson-4/scripts/commerce-app.js
+++ lesson-4/scripts/commerce-app.js
@@ -1,12 +1,21 @@
 const year = document.querySelector("#year");
 const lastTime = document.querySelector("#last-time");
+const currentTime = document.querySelector("#current");
 
 
 const options = {year: "numeric"}
-   
-year.innerHTML = new Date().toLocaleDateString("en-US", options);
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+
+year.innerHTML = new Date().toLocaleDateString("en-UK", options);
 
 lastTime.innerHTML =  new Date(document.lastModified);
+
+currentTime.innerHTML = `<em>${fulldate}</em>`;
+
+
 
 function toggleNav() {
     document.getElementById("menu").classList.toggle("open");
