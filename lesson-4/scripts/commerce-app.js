fee9013b7e01e0a41886752cc66fd75cb5e2ac32
Bookboy2000 <wil21131@byui.edu>
2023-01-27 18:21:55 -0700

    Fixing Hamburger menu and color contrast

--- lesson-4/scripts/commerce-app.js
+++ lesson-4/scripts/commerce-app.js
@@ -1,7 +1,7 @@
 const year = document.querySelector("#year");
 const lastTime = document.querySelector("#last-time");
 const currentTime = document.querySelector("#current");
-
+console.log("year")
 
 const options = {year: "numeric"}
 
@@ -20,7 +20,15 @@ currentTime.innerHTML = `<em>${fulldate}</em>`;
 function toggleNav() {
     document.getElementById("menu").classList.toggle("open");
     document.getElementById("hamburger").classList.toggle("new");
+    if(document.getElementById("menu").classList.contains("open")) {
+        document.querySelector(".test").classList.add("fix-date")
+        document.querySelector(".test").classList.remove("burger-date")
+    } else{
+        console.log("hello")
+        document.querySelector(".test").classList.add("burger-date")
+        document.querySelector(".test").classList.remove("fix-date")
+    }
 }
 
 const x = document.getElementById('hamburger')
-x.onclick = toggleNav();
+x.onclick = toggleNav;
