2679bde8e637c4b4ac2a23c59e001d213bc9f707
Bookboy2000 <wil21131@byui.edu>
2023-02-08 20:44:21 -0700

    Fixed BoM so that alma 5 is no longer a value
    Also added an if statement that will not allow an li to be added without a value.

--- lesson5/bom-assignment/scripts/bom-app.js
+++ lesson5/bom-assignment/scripts/bom-app.js
@@ -4,26 +4,28 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
+    if (input.value != '') {
+        const item = input.value;
+        input.value = '';
 
-    const item = input.value;
-    input.value = '';
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
+        listText.textContent = item
+        listItem.appendChild(listText);
 
-    listText.textContent = item
-    listItem.appendChild(listText);
+        listBtn.addEventListener('click', () => {
+            list.removeChild(listItem);
+        });
 
-    listBtn.addEventListener('click', () => {
-        list.removeChild(listItem);
-    });
+        listBtn.textContent = '❌';
+        listItem.appendChild(listBtn);
+        
+        list.appendChild(listItem);
 
-    listBtn.textContent = '❌';
-    listItem.appendChild(listBtn);
+        
+        input.focus();
+    }
     
-    list.appendChild(listItem);
-
-    
-    input.focus();
 });
