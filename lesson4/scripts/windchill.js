f20bc9ee009d17b46fc31dbf7083e27dc362a6d9
Bookboy2000 <wil21131@byui.edu>
2023-02-10 12:19:56 -0700

    changed my wind chill float into an int
    I also adjusted the padding on the news div for large view

--- lesson4/scripts/windchill.js
+++ lesson4/scripts/windchill.js
@@ -6,7 +6,7 @@ let t = parseInt(temp.textContent);
 let s = parseInt(speed.textContent);
 
 function calculateChill() {
-    chill.textContent = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 +0.4275 * t * s ** 0.16;
+    chill.textContent = parseInt(35.74 + 0.6215 * t - 35.75 * s ** 0.16 +0.4275 * t * s ** 0.16);
 }
 function checkNum() {
     if ((t < 50) && (s > 3)) {
