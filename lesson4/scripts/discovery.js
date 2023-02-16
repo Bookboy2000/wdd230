da6d1106a5d4c12178b6e595b1ea2281c7846d40
Bookboy2000 <wil21131@byui.edu>
2023-02-16 14:56:19 -0700

    Just adding things into discovery
    Also, I had to push my discovery css into the commerce css files

--- lesson4/scripts/discovery.js
+++ lesson4/scripts/discovery.js
@@ -1,11 +1,15 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
+    image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
-        image.removeAttribute("data-src");
+        image.removeAttribute('data-src');
     };
 };
 imagesToLoad.forEach((img) => {
     loadImages(img);
 });
+
+
+const visit = document.querySelector('#visit')
+
