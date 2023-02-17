464c246da4b1685d96070eaf34622487905a6857
Bookboy2000 <wil21131@byui.edu>
2023-02-16 20:45:17 -0700

    Added easier access to my discover page

--- lesson4/scripts/discovery.js
+++ lesson4/scripts/discovery.js
@@ -9,6 +9,8 @@ let oldDate = localStorage.getItem("oldDate")
 
 function compareDate() {
     if (oldDate != "") {
+        console.log(oldDate);
+        console.log(today);
         let outcome = (oldDate - today) / 84600000;
         let rounded = Math.round(outcome);
         visit.textContent = "It has been " + rounded + " days since you were here.";
