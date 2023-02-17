6efd7523aaa7210700b607e7dbb8490e338e75a9
Bookboy2000 <wil21131@byui.edu>
2023-02-17 14:40:41 -0700

    cleaned up my js for the discover page

--- lesson4/scripts/discovery.js
+++ lesson4/scripts/discovery.js
@@ -3,17 +3,18 @@ let visit = document.querySelector('#visit');
 
 let today = Date.now();
 
-localStorage.setItem("oldDate", today);
-
-let oldDate = localStorage.getItem("oldDate")
+let oldDate = localStorage.getItem("oldDate");
 
 function compareDate() {
-    console.log(oldDate);
-    console.log(today)
-    let outcome = (oldDate - today) / 84600000;
-    let rounded = Math.round(outcome);
-    visit.textContent = "It has been " + rounded + " days since you were here.";
-    console.log(rounded);
+    if (oldDate != 0) {
+        let outcome = (oldDate - today) / 84600000;
+        outcome = Math.abs(outcome);
+        let rounded = Math.round(outcome);
+        visit.textContent = "It has been " + rounded + " days since you were here."; 
+    };
+    
 };
 
 compareDate();
+
+localStorage.setItem("oldDate", today);
