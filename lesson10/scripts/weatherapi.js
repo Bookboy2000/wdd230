8d1ec99c72c4a60ed6728390e63440f58a8370f6
Bookboy2000 <wil21131@byui.edu>
2023-03-07 12:19:28 -0700

    Starting my weather API test page
    It may need a few hours for the API key to be ready

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q={fairbanks}&appid={5928f7cf482059dc544f74fe665b00a2}&units=imperial'
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+
+  function displayResults(weatherdata) {
+    currentTemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
