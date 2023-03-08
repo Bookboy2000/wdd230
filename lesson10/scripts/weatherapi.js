098efc7d55a72e68b442f619d1176cc0d04f1ca0
Bookboy2000 <wil21131@byui.edu>
2023-03-07 21:25:31 -0700

    I fixed the weatherData variable so it matches the other calls
    It was not the same throughout the page. I also styled the weather page.

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -2,14 +2,13 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q={fairbanks}&appid={5928f7cf482059dc544f74fe665b00a2}&units=imperial'
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=fairbanks&appid=5928f7cf482059dc544f74fe665b00a2&units=imperial'
 
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
         displayResults(data);
       } else {
           throw Error(await response.text());
@@ -22,8 +21,8 @@ async function apiFetch() {
   apiFetch();
 
 
-  function displayResults(weatherdata) {
-    currentTemp.innerHTML = `<strong>${weatherdata.main.temp.toFixed(0)}</strong>`;
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
