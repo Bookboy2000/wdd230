8927f5cd72bd5200821dea9366aca2fc6b6cb970
Bookboy2000 <wil21131@byui.edu>
2023-03-08 13:12:01 -0700

    Applied the weather API to my homepage
    I had to adjust the margins in different views to get the more specific layout I wanted from the weather div

--- st-george-c-of-c/scripts/windchill.js
+++ st-george-c-of-c/scripts/windchill.js
@@ -1,20 +1,43 @@
 const tempElement = document.querySelector('#temp');
 const speed = document.querySelector('#speed');
+const weatherIcon = document.querySelector('#weather-icon');
+const description = document.querySelector('#description')
 let chill = document.querySelector('#chill');
 
-let tempValue = parseInt(tempElement.textContent);
-let s = parseInt(speed.textContent);
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=saint george,ut,us&appid=5928f7cf482059dc544f74fe665b00a2&units=imperial'
 
-function calculateChill() {
-    chill.textContent = parseInt(35.74 + 0.6215 * tempValue - 35.75 * s ** 0.16 +0.4275 * tempValue * s ** 0.16);
+async function fetchApi() {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        results(data);  
+}
+
+fetchApi();
+
+function results(weather) {
+    tempElement.innerHTML = `${weather.main.temp.toFixed(0)}&#8457;`;
+    speed.innerHTML = `${weather.wind.speed.toFixed(2)}mph`;
+
+    const iconSrc = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    const desc = weather.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconSrc);
+    weatherIcon.setAttribute('alt', desc)
+    description.innerHTML = desc
+
+    checkNum();
+}
+
+function calculateChill(tempValue, speedValue) {
+    windChill = parseInt(35.74 + 0.6215 * tempValue - 35.75 * speedValue ** 0.16 +0.4275 * tempValue * speedValue ** 0.16)
+    chill.textContent = `${windChill}&#8457;`
 }
 function checkNum() {
-    if ((tempValue <= 50) && (s > 3)) {
-        calculateChill();
-    } else (
-        console.log('failed')
-    )
+    let tempValue = parseInt(tempElement.textContent);
+    let speedValue = parseInt(speed.textContent);
+
+    if ((tempValue <= 50) && (speedValue > 3)) {
+        calculateChill(tempValue, speedValue);
+    }
 }
-
-checkNum()
-
