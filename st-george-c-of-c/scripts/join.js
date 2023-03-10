3683fae77521293c45c86c90cfdf1c271852f230
Bookboy2000 <wil21131@byui.edu>
2023-03-09 20:26:16 -0700

    Trying to implement my teachers feed back by adding custom invalid messages

--- st-george-c-of-c/scripts/join.js
+++ st-george-c-of-c/scripts/join.js
@@ -1,3 +1,12 @@
 // Assigning the hidden input field "inputDate" the current date and time
 
 document.querySelector("#input-date").value = newday;
+
+const fName = document.querySelectorAll("input[name=fName]");
+
+// fName.addEventListener("input[name=fName]", () => {
+//     fName.setCustomValidity("");
+//     fName.checkValidity();
+//     console.log(fName.checkValidity());
+// })
+
