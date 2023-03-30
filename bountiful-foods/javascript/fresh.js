0d27537ec90aa7f9967f9dc8c13c0fa90e2757ba
Bookboy2000 <wil21131@byui.edu>
2023-03-29 18:26:30 -0600

    Another JavaScript update
    Just starting on the info card

--- bountiful-foods/javascript/fresh.js
+++ bountiful-foods/javascript/fresh.js
@@ -3,15 +3,19 @@ url = "https://brotherblazzard.github.io/canvas-content/fruit.json"
 async function getFruitData() {
     const response = await fetch(url);
     const data = await response.json();
-    buildOption(data);
+    buildOptions(data);
 };
 
 getFruitData();
 
-function buildOption(fruits) {
+function buildOptions(fruits) {
     const selectElement1 = document.querySelector('#fruit-1');
     const selectElement2 = document.querySelector('#fruit-2');
     const selectElement3 = document.querySelector('#fruit-3');
+    let empty1 = document.createElement('option');
+    selectElement2.appendChild(empty1);
+    let empty2 = document.createElement('option');
+    selectElement3.appendChild(empty2);
 
     fruits.forEach((fruit) => {
         let option1 = document.createElement('option');
@@ -23,31 +27,79 @@ function buildOption(fruits) {
 
         let option2 = document.createElement('option');
         let fruitName2 = document.createElement('span');
-        let empty1 = document.createElement('option');
         option2.setAttribute('value', fruit.name);
         fruitName2.innerHTML = fruit.name;
         option2.appendChild(fruitName2);
-        selectElement2.appendChild(empty1);
         selectElement2.appendChild(option2);
 
         let option3 = document.createElement('option');
         let fruitName3 = document.createElement('span');
-        let empty2 = document.createElement('option');
         option3.setAttribute('value', fruit.name);
         fruitName3.innerHTML = fruit.name;
         option3.appendChild(fruitName3);
-        selectElement3.appendChild(empty2);
         selectElement3.appendChild(option3);
-      
-        
     });
+
+    const drinkBtn = document.querySelector('.drink-btn');
+
+    drinkBtn.addEventListener('click', () => {
+        const fName = document.querySelector('.f-name');
+        let paraName = document.querySelector('.para-name');
+        paraName.textContent = fName.value;
+    
+        const email = document.querySelector('.email');
+        let paraEmail = document.querySelector('.para-email');
+        paraEmail.textContent = email.value;
+    
+        const phone = document.querySelector('.phone');
+        let paraPhone = document.querySelector('.para-phone');
+        paraPhone.textContent = phone.value;
+    
+        const instruct = document.querySelector('.instruct');
+        let paraInstruct = document.querySelector('.para-instruct');
+        paraInstruct.textContent = instruct.value;
+    
+        const fruit1 = document.querySelector('#fruit-1');
+        const fruit2 = document.querySelector('#fruit-2');
+        const fruit3 = document.querySelector('#fruit-3');
+        const choice1 = document.querySelector('.choice-1');
+        const choice2 = document.querySelector('.choice-2');
+        const choice3 = document.querySelector('.choice-3');
+        const date = document.querySelector('.order-date');
+        const carb = document.querySelector('.carbs');
+        const protein = document.querySelector('.protein');
+        const fat = document.querySelector('.fat');
+        const sugar = document.querySelector('.sugar');
+        const calories = document.querySelector('.calories');
+        
+    
+        fruits.forEach((fruit) => {
+            if (fruit.name == fruit1.value) {
+                choice1.innerHTML = fruit.name;
+                const carb1 = fruit.nutritions.carbohydrates;
+                const protein1 = fruit.nutritions.protein;
+                const fat1 = fruit.nutritions.fat;
+                const sugar1 = fruit.nutritions.sugar;
+                const calories1 = fruit.nutritions.calories;
+                
+            } else if (fruit.name == fruit2.value) {
+                choice2.innerHTML = fruit.name;
+                const carb2 = fruit.nutritions.carbohydrates;
+                const protein2 = fruit.nutritions.protein;
+                const fat2 = fruit.nutritions.fat;
+                const sugar2 = fruit.nutritions.sugar;
+                const calories2 = fruit.nutritions.calories;
+
+            } else if (fruit.name == fruit3.value) {
+                choice3.innerHTML = fruit.name;
+                const carb3 = fruit.nutritions.carbohydrates;
+                const protein3 = fruit.nutritions.protein;
+                const fat3 = fruit.nutritions.fat;
+                const sugar3 = fruit.nutritions.sugar;
+                const calories3 = fruit.nutritions.calories;
+            };
+        });
+    }); 
 };
-
-const drinkBtn = document.querySelector('.drink-btn');
-
-drinkBtn.addEventListener('click', () => {
-    let fName = document.querySelector('.fname');
-    fName.innerHTML = 
-});
 
 
