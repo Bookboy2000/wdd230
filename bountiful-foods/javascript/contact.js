7c5c9205e9e14ba1c4742bff7f0cb0569de9d575
Bookboy2000 <wil21131@byui.edu>
2023-04-04 15:42:31 -0600

    Finished the Bountiful foods site
    I just need to validate everything

--- bountiful-foods/javascript/contact.js
+++ bountiful-foods/javascript/contact.js
@@ -1,2 +1,21 @@
 lastTime = document.querySelector('.last-mod');
 lastTime.innerHTML = new Date(document.lastModified);
+
+questionBtn = document.querySelector('.question-btn');
+
+questionBtn.addEventListener('click', () => {
+    form = document.getElementById('question-form');
+    if (form.reportValidity() != true) {
+        document.getElementById('question-bad').classList.add('question-invalid');
+        document.querySelector('.question-invalid').classList.remove('question-valid')
+    } else {
+        document.getElementById('question-good').classList.add('question-here');
+        document.querySelector('.question-here').classList.remove('question-gone')
+
+        if (document.getElementById('question-bad').classList.contains('question-invalid')) {
+            document.querySelector('.question-invalid').classList.add('question-valid');
+            document.querySelector('.question-invalid').classList.remove('question-invalid');
+        } else {
+        };
+    }
+})
